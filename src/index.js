644d44a1d59e48380e284e0342454f9ed0a385d3
Artem <artic12101997dr@gmail.com>
2023-01-08 20:34:10 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import { fetchApi } from "./js/fetchApi";
 import Notiflix from "notiflix";
+
+
+let inputValue = '';
+let identificator = '';
+let type = '';
+
+async function getDrink() {
+	const data = await fetchApi(type, identificator, inputValue);
+	console.log(data);
+}
+
+getDrink();
--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-export async function fetchApi(type, identificator, drink) {
-    try {
-        const response = await axios.get(
-            `https://www.thecocktaildb.com/api/json/v1/1/${type}.php?${identificator}${drink}`
-        );
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+export async function fetchApi(type, identificator, inputValue) {
+  try {
+    const response = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/${type}.php?${identificator}${inputValue}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // export function fetchApi(type, identificator, drink) {
