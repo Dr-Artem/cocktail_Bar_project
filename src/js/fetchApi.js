7ff68220af60ea2f369f0a23b0ce9af2cf974ad3
SunAndrea <andriishtohryn@gmail.com>
2023-01-10 10:18:34 +0200

    make murkup

--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export async function fetchApi(type, identificator, inputValue) {
-  try {
-    const response = await axios.get(
-      `https://www.thecocktaildb.com/api/json/v1/1/${type}.php?${identificator}${inputValue}`
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
+// export async function fetchApi(type, identificator, inputValue) {
+//   try {
+//     const response = await axios.get(
+//       `https://www.thecocktaildb.com/api/json/v1/1/${type}.php?${identificator}${inputValue}`
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 // export function fetchApi(type, identificator, drink) {
 //   return fetch(
@@ -21,3 +21,17 @@ export async function fetchApi(type, identificator, inputValue) {
 //     return response.json();
 //   });
 // }
+import axios from 'axios';
+
+export async function fetchApi(type, identificator, inputValue) {
+  try {
+    const response = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=campari
+
+`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
