6f734316c96ff3cf02ab230a972bcc3238a5dde6
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-01-10 12:54:33 +0200

    добавив картки

--- src/index.js
+++ src/index.js
@@ -12,23 +12,3 @@ async function getDrink(el) {
 }
 
 // getDrink();
-const tablet = window.matchMedia('(min-width: 768px)');
-const desctop = window.matchMedia('(min-width: 1280px)');
-let num = 3;
-
-if (window.matchMedia('(min-width: 1280px)').matches) {
-  num = 9;
-} else if (window.matchMedia('(min-width: 768px)').matches) {
-  num = 6;
-}
-
-for (let index = 0; index < num; index++) {
-  // el.matches && (num = 6);
-  getRandomDrink('random');
-  console.log(num);
-}
-
-// function handleDeviceChange(e) {
-//   if (e.matches) outputElement.textContent = 'Bigger Than Mobile';
-//   else outputElement.textContent = 'Mobile';
-// }
