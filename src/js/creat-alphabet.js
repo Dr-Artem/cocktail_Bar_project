abfdd6311c3f941f8c583aeafd94adac7337237f
Artem <artic12101997dr@gmail.com>
2023-01-12 01:38:24 +0200

    header

--- src/js/creat-alphabet.js
+++ src/js/creat-alphabet.js
@@ -32,7 +32,7 @@ const alphabet = [
   'Y',
   'Z',
 ];
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const abcArray = [...alphabet, ...numbers];
 
 function creatBTN() {
--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,48 @@
-const btnOpenModal = document.querySelector('.js-modal-open');
-console.log(btnOpenModal);
-const modalEl = document.querySelector('.js-modal');
-console.log(modalEl);
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
 
-function handleOpenModal() {
-  modalEl.classList.toggle('hidden');
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+
+  let subMenu = document.querySelector('.menu__navbar-link-favourite');
+  subMenu.addEventListener('click', function (event) {
+    subMenu.parentElement.classList.toggle('_active');
+  });
+} else {
+  document.body.classList.add('_pc');
 }
 
-btnOpenModal.addEventListener('click', handleOpenModal);
+const burger = document.querySelector('.menu-icon');
+
+if (burger) {
+  const burgerMenu = document.querySelector('.header__menu');
+  burger.addEventListener('click', () => {
+    document.body.classList.toggle('_lock');
+    burger.classList.toggle('_active');
+    burgerMenu.classList.toggle('_active');
+  });
+}
