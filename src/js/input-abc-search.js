c24d79791bfb7e2529eff69398a8dda0071fc754
Анастасія Савченко <savyle4ka13@gmail.com>
2023-01-13 00:42:58 +0000

    add main + darck styles for hero, main, footer

--- src/js/input-abc-search.js
+++ src/js/input-abc-search.js
@@ -1,11 +1,17 @@
 import { fetchApi } from './fetchApi';
 import card from '../templates/renderCocktailCards.hbs';
 import { refs } from './abc-search';
+import { scroll } from './abc-search';
+import { hiddenTitle } from './abc-search';
+import { addTitle } from './abc-search';
 import Notiflix from 'notiflix';
+import { preloader } from './abc-search';
+import { scrollTobottom } from './abc-search';
 
 let inputValue = '';
 let identificator = '';
 let type = '';
+let timerId = '';
 
 refs.inputBtn.addEventListener('click', onInputClick);
 refs.btnDropdown.addEventListener('click', onbtnDropdownClick);
@@ -15,17 +21,27 @@ function onInputClick(e) {
   refs.btnDropdown.style.display = 'block';
 }
 
-async function onbtnDropdownClick(e) {
+function onbtnDropdownClick(e) {
   e.preventDefault;
   refs.mainSection.innerHTML = '';
+  hiddenTitle();
 
   const element = e.target.textContent;
   identificator = 'f=';
   type = 'search';
   inputValue = element;
 
-  test(element);
+  inputChange(element);
 
+  preloader();
+
+  timerId = setTimeout(getCoctails, 2000);
+}
+
+clearTimeout(timerId);
+
+async function getCoctails() {
+  preloader();
   const data = await fetchApi(type, identificator, inputValue);
   console.log(data);
 
@@ -33,18 +49,21 @@ async function onbtnDropdownClick(e) {
     refs.mainSection.insertAdjacentHTML('beforeend', card(data.drinks));
 
     if (!data.drinks) {
+      addTitle();
+      scrollTobottom();
       return Notiflix.Notify.failure(refs.failureMessage);
     }
 
     Notiflix.Notify.success(
-      'Hooray! We found' + ` ${data.drinks.length} ` + 'cocktails!'
+      'We found' + ` ${data.drinks.length} ` + 'cocktails for you!'
     );
+    scroll();
   } catch (err) {
     console.log(err);
   }
 }
 
-function test(element) {
+function inputChange(element) {
   inputValue = element;
   refs.inputDropdown.value = element;
   refs.inputDropdown.style.backgroundColor = 'var(--accent-text-color)';
