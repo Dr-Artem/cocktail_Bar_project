30128ef69ea99cf331261cf3e83673bd39a55c1d
Artem <artic12101997dr@gmail.com>
2023-01-15 18:49:15 +0200

    add favourite cocktail

--- src/js/input-abc-search.js
+++ src/js/input-abc-search.js
@@ -1,75 +1,74 @@
-import { fetchApi } from './fetchApi';
-import card from '../templates/renderCocktailCards.hbs';
-import { refs } from './abc-search';
-import { scroll } from './abc-search';
-import { hiddenTitle } from './abc-search';
-import { addTitle } from './abc-search';
-import Notiflix from 'notiflix';
-import { preloader } from './abc-search';
-import { scrollTobottom } from './abc-search';
+// import { fetchApi } from './fetchApi';
+// import card from '../templates/renderCocktailCards.hbs';
+// import { refs } from '..';
+// import { scroll } from '..';
+// import { params } from '..';
+// import { hiddenTitle } from '..';
+// import { addTitle } from '..';
+// import Notiflix from 'notiflix';
+// import { preloader } from '..';
+// import { scrollTobottom } from '..';
 
-let inputValue = '';
-let identificator = '';
-let type = '';
-let timerId = '';
+// let inputValue = '';
+// let identificator = '';
+// let type = '';
+// let timerId = '';
 
-refs.inputBtn.addEventListener('click', onInputClick);
-refs.btnDropdown.addEventListener('click', onbtnDropdownClick);
+// refs.inputBtn.addEventListener('click', onInputClick);
+// refs.btnDropdown.addEventListener('click', onbtnDropdownClick);
 
-function onInputClick(e) {
-  e.preventDefault();
-  refs.btnDropdown.style.display = 'block';
-}
+// function onInputClick(e) {
+//     e.preventDefault();
+//     refs.btnDropdown.style.display = 'block';
+// }
 
-function onbtnDropdownClick(e) {
-  e.preventDefault;
-  refs.mainSection.innerHTML = '';
-  hiddenTitle();
+// function onbtnDropdownClick(e) {
+//     e.preventDefault;
+//     params.cocktailSection.innerHTML = '';
+//     hiddenTitle();
 
-  const element = e.target.textContent;
-  identificator = 'f=';
-  type = 'search';
-  inputValue = element;
+//     const element = e.target.textContent;
+//     identificator = 'f=';
+//     type = 'search';
+//     inputValue = element;
 
-  inputChange(element);
+//     inputChange(element);
 
-  preloader();
+//     preloader();
 
-  timerId = setTimeout(getCoctails, 2000);
-}
+//     timerId = setTimeout(getCoctails, 2000);
+// }
 
-clearTimeout(timerId);
+// clearTimeout(timerId);
 
-async function getCoctails() {
-  preloader();
-  const data = await fetchApi(type, identificator, inputValue);
-  console.log(data);
+// async function getCoctails() {
+//     preloader();
+//     const data = await fetchApi(type, identificator, inputValue);
+//     console.log(data);
 
-  try {
-    refs.mainSection.insertAdjacentHTML('beforeend', card(data.drinks));
+//     try {
+//         params.cocktailSection.innerHTML = card(data.drinks);
 
-    if (!data.drinks) {
-      addTitle();
-      scrollTobottom();
-      return Notiflix.Notify.failure(refs.failureMessage);
-    }
+//         if (!data.drinks) {
+//             addTitle();
+//             scrollTobottom();
+//             return Notiflix.Notify.failure(refs.failureMessage);
+//         }
 
-    Notiflix.Notify.success(
-      'We found' + ` ${data.drinks.length} ` + 'cocktails for you!'
-    );
-    scroll();
-  } catch (err) {
-    console.log(err);
-  }
-}
+//         Notiflix.Notify.success('We found' + ` ${data.drinks.length} ` + 'cocktails for you!');
+//         scroll();
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
 
-function inputChange(element) {
-  inputValue = element;
-  refs.inputDropdown.value = element;
+// function inputChange(element) {
+//     inputValue = element;
+//     refs.inputDropdown.value = element;
 
-  refs.inputDropdown.style.backgroundColor = 'var(--accent-text-color)';
-  refs.inputDropdown.style.color = 'var(--main-white-color)';
-  refs.icon.style.fill = 'var(--main-white-color)';
+//     refs.inputDropdown.style.backgroundColor = 'var(--accent-text-color)';
+//     refs.inputDropdown.style.color = 'var(--main-white-color)';
+//     refs.icon.style.fill = 'var(--main-white-color)';
 
-  refs.btnDropdown.style.display = 'none';
-}
+//     refs.btnDropdown.style.display = 'none';
+// }
--- src/js/modal_window.js
+++ src/js/modal_window.js
@@ -1,6 +1,7 @@
 import modalWindowAdd from '../templates/modal.hbs';
 import modalWindowIngredient from '../templates/renderIngridientInfo.hbs';
 import { fetchApi } from './fetchApi';
+import { params } from '..';
 
 const backdrop = document.querySelector('.backdrop');
 const backdropIngridient = document.querySelector('.backdrop-ingridient');
@@ -10,83 +11,100 @@ const modalWindowIngridient = document.querySelector('[data-ingridient]');
 const modal = document.querySelector('[data-modal]');
 
 function openModalWindow(el) {
-  let inputValue = el.target.name;
-  let identificator = 's=';
-  let type = 'search';
-
-  fetchApi(type, identificator, inputValue)
-    .then(response => {
-      if (el.target.className !== 'buttons__learn-more') {
+    let inputValue = el.target.id;
+    let identificator = 'i=';
+    let type = 'lookup';
+    let localKeys = [];
+    for (const key in localStorage) {
+        localKeys.push(key);
+    }
+    if (el.target.className !== 'buttons__learn-more') {
         return;
-      }
+    }
 
-      modalWindow.insertAdjacentHTML(
-        'beforeend',
-        modalWindowAdd(response.drinks)
-      );
+    fetchApi(type, identificator, inputValue)
+        .then(response => {
+            const cardItem = document.querySelectorAll('.cocktail__item');
+            let itemId = `strDrink${response.drinks[0].idDrink}`;
+            modalWindow.innerHTML = modalWindowAdd(response.drinks);
+            let favouriteBtn = document.querySelector('.favorite-btn__text');
 
-      const ingridientList = document.querySelector('.ingridients__list');
+            favouriteBtn.onclick = function () {
+                if (localKeys.includes(itemId)) {
+                    localStorage.removeItem(`${itemId}`);
+                    favouriteBtn.textContent = 'Add to favourite';
+                    localIndex = localKeys.indexOf(`${itemId}`);
+                    localKeys.splice(localIndex, 1);
+                } else {
+                    let parentLi;
+                    cardItem.forEach(item => {
+                        if (item.id === itemId) {
+                            parentLi = item.innerHTML;
+                        }
+                    });
+                    localStorage.setItem(`${itemId}`, parentLi);
+                    localKeys.push(itemId);
+                    favouriteBtn.textContent = 'Remove from favourite';
+                }
+            };
 
-      for (let i = 0; i < 15; i++) {
-        const ingridient = response.drinks[0][`strIngredient${i}`];
-        if (ingridient) {
-          ingridientList.insertAdjacentHTML(
-            'beforeend',
-            `<li> <a class="ingridients__link" name='${ingridient}'>&#10038${ingridient}</a></li>`
-          );
-        }
-      }
-      backdrop.classList.remove('hidden');
-      document.body.style.overflow = 'hidden';
-    })
-    .catch(error => {
-      console.log(error);
-    });
+            const ingridientList = document.querySelector('.ingridients__list');
+
+            for (let i = 1; i <= 15; i++) {
+                const ingridient = response.drinks[0][`strIngredient${i}`];
+                if (ingridient) {
+                    ingridientList.innerHTML = `<li> <a class="ingridients__link" name='${ingridient}'>&#10038${ingridient}</a></li>`;
+                }
+            }
+            backdrop.classList.remove('hidden');
+            document.body.style.overflow = 'hidden';
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
 function closeModalWindow(el) {
-  if (el.target.className.animVal !== 'button__icon') {
-    return;
-  }
-  document.body.style.overflow = '';
-  modalWindow.innerHTML = '';
-  backdrop.classList.add('hidden');
+    if (el.target.className.animVal !== 'button__icon') {
+        return;
+    }
+    document.body.style.overflow = '';
+    modalWindow.innerHTML = '';
+    backdrop.classList.add('hidden');
 }
 
 function openIngridientModalWindow(el) {
-  let inputValue = el.target.name;
-  let identificator = 'i=';
-  let type = 'search';
-  fetchApi(type, identificator, inputValue).then(response => {
-    if (el.target.nodeName !== 'A') {
-      return;
-    }
-    if (!response.ingredients[0].strDescription) {
-      return;
-    }
+    let inputValue = el.target.name;
+    let identificator = 'i=';
+    let type = 'search';
+    fetchApi(type, identificator, inputValue).then(response => {
+        if (el.target.nodeName !== 'A') {
+            return;
+        }
+        if (!response.ingredients[0].strDescription) {
+            return;
+        }
+        modalWindowIngridient.innerHTML = modalWindowIngredient(response.ingredients);
 
-    modalWindowIngridient.innerHTML = modalWindowIngredient(
-      response.ingredients
-    );
-    backdrop.classList.add('hidden');
-    backdropIngridient.classList.remove('hidden');
-  });
+        backdrop.classList.add('hidden');
+        backdropIngridient.classList.remove('hidden');
+
+        let favouriteBtnIngredient = document.querySelector('.ingredient__btn');
+        favouriteBtnIngredient.onclick = function (params) {};
+    });
 }
 
 function closeIngridientModalWindow(el) {
-  if (
-    el.target.className.animVal !== 'button__icon__ingredient' &&
-    el.target.className !== 'backdrop-ingridient'
-  ) {
-    return;
-  }
-  modalWindowIngridient.innerHTML = '';
+    if (el.target.className.animVal !== 'button__icon__ingredient' && el.target.className !== 'backdrop-ingridient') {
+        return;
+    }
+    modalWindowIngridient.innerHTML = '';
 
-  backdrop.classList.remove('hidden');
-  backdropIngridient.classList.add('hidden');
+    backdrop.classList.remove('hidden');
+    backdropIngridient.classList.add('hidden');
 }
 
 modal.addEventListener('click', closeModalWindow);
 modal.addEventListener('click', openIngridientModalWindow);
-document.body.addEventListener('click', openModalWindow);
+params.cocktailSection.addEventListener('click', openModalWindow);
 backdropIngridient.addEventListener('click', closeIngridientModalWindow);
